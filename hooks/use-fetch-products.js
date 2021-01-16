389f526405f7ac08b5b61ac54b47a7b9cb6f6073
abimaelandrade <abimaelc.andrade@gmail.com>
2021-01-15 23:39:14 -0300

    feat: hook useFatchProducts

---
+++ hooks/use-fetch-products.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useFetchProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    let mounted = true;
+
+    axios
+      .get('/api/products')
+      .then((res) => mounted && setProducts(res.data.products))
+      .catch((error) => mounted && setError(true));
+
+      return () => (mounted = false)
+  },[]);
+
+  return [products, error];
+}
